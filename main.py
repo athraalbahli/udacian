529d999274de01b55d201cbb68be77b5c5e549d7
Athra Albahli <athraalbahli@gmail.com>
2018-10-26 19:06:56 +0300

    form code

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-class Udacian:
-	def __init__(self,name,city, enrollment, nanodegree, status):
-		self.name = name
-		self.city = city
-		self.enrollment = enrollment
-		self.nanodegree = nanodegree
-		self.status = status
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
 
-	def print_string(self):
-		print("my name is {0} in {1} studing {2} nanodegree with Ms {3} , she is {4}" . format(self.name, self.city,self.enrollment, self.nanodegree,self.status))
+memory = []
+form = '''<!DOCTYPE html>
+  <title>Udacian</title>
+  <form method="POST" action="http://localhost:8000/">
+    <textarea name="name">name</textarea>
+    <br>
+    <textarea name="city">city</textarea>
+    <br>
+    <textarea name="enrollment">enrollment</textarea>
+    <br>
+    <textarea name="nanodegree">nanodegree</textarea>
+    <br>
+    <textarea name="status">status</textarea>
+    <br>
+    <button type="submit">Post it!</button>
+  </form>
+  <pre>
+{}
+  </pre>
+'''
 
 
-athra = Udacian("Athra" , "Dammam" , "Full Stack Web Developer" , "Elham" , "on track")
-athra.print_string()
+class MessageHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+
+      length = int(self.headers.get('Content-length', 0))
+      body = self.rfile.read(length).decode()
+      params = parse_qs(body) 
+
+      # Send the "message" field back as the response.
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain; charset=utf-8')
+      self.end_headers()
+      self.wfile.write("my name is {} in {} studing {} nanodegree with Ms {} , she is {}".format(params["name"][0],params["city"][0],params["enrollment"][0], params["nanodegree"][0],params["status"][0]).encode())
+      # self._set_headers()
+
+    def do_GET(self):
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html; charset=utf-8')
+      self.end_headers()
+		  # Send the html message
+      self.wfile.write(form.encode())
+
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MessageHandler)
+    httpd.serve_forever()
